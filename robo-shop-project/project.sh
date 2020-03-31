6b1cc376a9b0590390149b93a17d33da930e7374
Chandralekha.s <chandralekha.s@sophos.com>
2020-03-31 22:39:27 +0530

    "Added more services"

--- robo-shop-project/project.sh
+++ robo-shop-project/project.sh
@@ -86,4 +86,49 @@ STAT $? "Setting Up YUM Repository"
 yum install -y mongodb-org &>>$LOG_FILE
 STAT $? "Installing MONGODB"
 
+systemctl enable mongod
+systemctl start mongod 
+STAT $? "Starting MongoDB Service"
+
+SERVICE_NAME=RABBITMQ
+LOGGER INFO "Starting RABBITMQ Setup"
+yum list installed | grep erlang &>/dev/null
+
+case $? in
+    0)
+      STAT SKIP "Installing ErLang Package"
+      ;;
+    *)
+      yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y &>>$LOG_FILE
+      STAT $? "Installing ErLang Package"
+      ;;
+esac
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG_FILE
+STAT $? "Setting up Yum repositories"
+
+yum install rabbitmq-server -y &>>$LOG_FILE 
+STAT $? "Installing RabbitMQ Server"
+
+systemctl enable rabbitmq-server &>>$LOG_FILE 
+systemctl start rabbitmq-server &>>$LOG_FILE 
+STAT $? "Start RabbitMq Server"
+
+
+SERVICE_NAME=MYSQL
+LOGGER INFO "Starting Mysql setup"
+LOGGER INFO "Downloading MYSQL"
+yum remove mariadb-libs -y &>/dev/null
+wget https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar&>>$LOG_FILE
+STAT $? "Downloaded MYSQL"
+tar -xf mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar &>/dev/null
+yum install mysql-community-client-5.7.28-1.el7.x86_64.rpm \
+              mysql-community-common-5.7.28-1.el7.x86_64.rpm \
+              mysql-community-libs-5.7.28-1.el7.x86_64.rpm \
+              mysql-community-server-5.7.28-1.el7.x86_64.rpm -y &>>$LOG_FILE
+STAT $? "Installing MYSQL"
+systemctl enable mysqld &>>$LOG_FILE
+systemctl start mysqld &>>$LOG_FILE
+STAT $? "Starting MYSQL"
+
 
